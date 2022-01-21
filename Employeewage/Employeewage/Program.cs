6bc37907d43e7db8b1445b6f21abb555179b8461
rahul <rahulravi2502@gmail.com>
2022-01-21 23:57:36 +0530

    git commit -m "modify"

--- Employeewage/Employeewage/Program.cs
+++ Employeewage/Employeewage/Program.cs
@@ -15,7 +15,8 @@ namespace Employeewage
             // Employee1.EmployeewageUc2();
             // Employee2.EmployeewageUc3();
             // Employee3.EmployeewageUc4();
-            Employee4.EmployeewageUc4();
+            //Employee4.EmployeewageUc5();
+            Employee5.EmployeewageUc6();
 
             Console.ReadLine();
         }
