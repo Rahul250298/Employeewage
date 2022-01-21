180f7d0908c7d176a67797257ac3e9eb3c1d286a
rahul <rahulravi2502@gmail.com>
2022-01-21 23:33:51 +0530

    git add "modification"

--- Employeewage/Employeewage/Program.cs
+++ Employeewage/Employeewage/Program.cs
@@ -10,13 +10,14 @@ namespace Employeewage
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Welcome to Employee Wage Computation");
             //Employee.Employeeattendance();
             // Employee1.EmployeewageUc2();
-           // Employee2.EmployeewageUc3();
-            Console.WriteLine("Welcome to Employee Wage Computation");
-            Employee3.EmployeewageUc4();
+            // Employee2.EmployeewageUc3();
+            // Employee3.EmployeewageUc4();
+            Employee4.EmployeewageUc4();
 
-                Console.ReadLine();
+            Console.ReadLine();
         }
     }
 }
