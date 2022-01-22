0f7fd5ae7a5ae0f9916da00756a426a57e797597
Rahul250298 <91786240+Rahul250298@users.noreply.github.com>
2022-01-22 11:24:29 +0530

    main program

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employeewage
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Wage Computation");
+            //Employee.Employeeattendance();
+            // Employee1.EmployeewageUc2();
+            // Employee2.EmployeewageUc3();
+            // Employee3.EmployeewageUc4();
+            //Employee4.EmployeewageUc5();
+            Employee5.EmployeewageUc6();
+
+            Console.ReadLine();
+        }
+    }
+}
