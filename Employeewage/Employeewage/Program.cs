ba167f648731666da78d2801776d2216e082ea80
Rahul R <91786240+Rahul250298@users.noreply.github.com>
2022-01-30 06:32:45 +0530

    s

--- Employeewage/Employeewage/Program.cs
+++ Employeewage/Employeewage/Program.cs
@@ -12,11 +12,12 @@ namespace Employeewage
         {
             Console.WriteLine("Welcome to Employee Wage Computation");
             //Employee.Employeeattendance();
-            // Employee1.EmployeewageUc2();
-            // Employee2.EmployeewageUc3();
-            // Employee3.EmployeewageUc4();
+            //Employee1.EmployeewageUc2();
+            //Employee2.EmployeewageUc3();
+            //Employee3.EmployeewageUc4();
             //Employee4.EmployeewageUc5();
-            Employee5.EmployeewageUc6();
+            //Employee5.EmployeewageUc6();
+              Employee6.EmployeewageUc7();
 
             Console.ReadLine();
         }
